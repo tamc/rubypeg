d4c6cbb14febf5d590ee7f2b2f42d616def37637
tamc <tamc@greenonblack.com>
2010-02-01 22:19:22 +0000

    Added an example of a parser for more PEG like grammar text files

---
+++ examples/original_peg/original_peg.rb
@@ -0,0 +1,145 @@
+require 'peg_leg'
+
+class OriginalPeg < PegLeg
+  
+  def root
+    node :grammar do 
+      any_number_of { spacing && ( nodes || definitions ) }
+    end
+  end
+
+  def nodes
+    node :node do
+      identifier && assigns && expression && end_of_line
+    end
+  end
+  
+  def definitions
+    node :definition do
+      identifier && equals && expression && end_of_line
+    end
+  end
+  
+  def identifier
+    terminal(/[a-zA-Z_][a-zA-Z0-9]*/) && spacing
+  end
+  
+  def assigns
+    ignore { terminal(':=') } && spacing
+  end
+  
+  def equals
+    ignore { terminal('=') } && spacing
+  end
+  
+  def expression
+    alternatives || _sequences
+  end
+  
+  def _sequences
+    node :sequence do
+      one_or_more { elements && spacing }
+    end
+  end
+  
+  def alternatives
+    node :alternatives do
+      elements && one_or_more { divider && elements }
+    end
+  end
+
+  def divider
+    ignore { terminal('|') } && spacing
+  end
+  
+  def elements
+    ignored || negative_lookahead_element || positive_lookahead_element || optional_element || any_number_of_element || one_or_more_element || element
+  end
+  
+  def ignored
+    node :ignored do 
+      ignore { terminal('`') } && element
+    end
+  end
+  
+  def negative_lookahead_element
+    node :not_followed_by do
+      ignore { terminal('!') } && element
+    end
+  end
+  
+  def positive_lookahead_element
+    node :followed_by do
+      ignore { terminal('&') } && element
+    end
+  end
+  
+  def optional_element
+    node :optional do 
+      element && ignore { terminal('?') }
+    end
+  end
+  
+  def any_number_of_element
+    node :any_number_of do 
+      element && ignore { terminal('*') }
+    end
+  end
+  
+  def one_or_more_element
+    node :one_or_more do
+      element && ignore { terminal('+') }
+    end
+  end
+  
+  def element
+    bracketed_expression || identifier || terminal_string || terminal_regexp || any_character
+  end
+  
+  def bracketed_expression
+     open_bracket && expression && close_bracket
+  end
+  
+  def open_bracket
+    ignore { terminal ('(')} && spacing
+  end
+  
+  def close_bracket
+    ignore { terminal (')')} && spacing
+  end
+  
+  def terminal_string
+    node :terminal_string do
+      single_quoted_string || double_quoted_string
+    end
+  end
+    
+  def single_quoted_string
+    ignore { terminal("'") } && terminal(/[^'']*/) && ignore { terminal("'") } && spacing
+  end
+  
+  def double_quoted_string
+    ignore { terminal('"') } && terminal(/[^"]*/) && ignore { terminal('"') } && spacing
+  end
+  
+  def terminal_regexp
+    node :terminal_regexp do
+      terminal(/\[[a-zA-Z\-0-9]*\]/) && spacing
+    end
+  end
+  
+  def any_character
+    node :any_character do 
+      ignore { terminal('.') } && spacing
+    end
+  end
+  
+  def end_of_line
+    ignore { terminal(/[\n\r]+/) }
+  end
+  
+  def spacing
+    ignore { terminal(/[ \t]*/)  }
+  end
+  
+end
---
+++ examples/original_peg/original_peg_spec.rb
@@ -0,0 +1,104 @@
+$:.unshift File.join(File.dirname(__FILE__), *%w[.])
+$:.unshift File.join(File.dirname(__FILE__), *%w[.. .. lib])
+require 'peg_leg'
+require 'original_peg'
+
+describe OriginalPeg do
+  
+it "parses rules, one per line" do
+input = <<END
+  one = .
+  two = .
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:any_character]]],[:definition,"two",[:sequence,[:any_character]]]]
+end
+
+it "assigns nodes, one per line" do
+input = <<END
+  one := .
+  two := .
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:node,"one",[:sequence,[:any_character]]],[:node,"two",[:sequence,[:any_character]]]]
+end
+
+it "parses terminal strings in double quotes" do
+input = <<END
+  one = "terminal string"
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:terminal_string,"terminal string"]]]]
+end
+
+it "parses terminal strings in single quotes" do
+input = <<END
+  one = 'terminal string'
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:terminal_string,"terminal string"]]]]
+end
+
+
+it "parses terminal character matches" do
+input = <<END
+  one = [a-z]
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:terminal_regexp,"[a-z]"]]]]
+end
+
+it "parses any character matches" do
+input = <<END
+  one = .
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:any_character]]]]
+end
+
+it "parses alternatives" do
+input = <<END
+  one = "one" | "two" | "three"
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:alternatives,[:terminal_string,"one"],[:terminal_string,"two"],[:terminal_string,"three"]]]]
+end
+
+it "parses sequences" do
+input = <<END
+  one = "one" "two" "three"
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:terminal_string,"one"],[:terminal_string,"two"],[:terminal_string,"three"]]]]
+
+end
+
+it "parses alternatives in sequences" do
+input = <<END
+  one = ("one"|"1") "two" ( "three" | "3" )
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence, [:alternatives,[:terminal_string,"one"],[:terminal_string,'1']],[:terminal_string,"two"],[:alternatives,[:terminal_string,"three"],[:terminal_string,'3']]]]]
+
+end
+
+it "parses sequences in alternatives" do
+input = <<END
+  one = ( "one" "two" "three" ) | ("1" "2" "3")
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:alternatives,[:sequence,[:terminal_string,"one"],[:terminal_string,"two"],[:terminal_string,"three"]],[:sequence,[:terminal_string,'1'],[:terminal_string,'2'],[:terminal_string,'3']]]]]
+end
+
+it "parses suffix elements" do
+input = <<END
+  one = "one"? "two"* "three"+ ("four" | "five")?
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:optional,[:terminal_string,"one"]],[:any_number_of,[:terminal_string,"two"]],[:one_or_more,[:terminal_string,"three"]],[:optional,[:alternatives,[:terminal_string,"four"],[:terminal_string,"five"]]]]]]
+end
+
+it "parses lookaheads" do
+input = <<END
+  one = !"one" &"two"
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:not_followed_by,[:terminal_string,"one"]],[:followed_by,[:terminal_string,"two"]]]]]
+end
+
+
+it "parses ignores" do
+input = <<END
+  one = `"one" &"two"
+END
+OriginalPeg.parse(input).to_ast.should == [:grammar,[:definition,"one",[:sequence,[:ignored,[:terminal_string,"one"]],[:followed_by,[:terminal_string,"two"]]]]]
+end
+end
