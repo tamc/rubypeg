77db8f5425f1decbeebeb950d472c0a7daf1ed0f
tamc <tamc@greenonblack.com>
2010-02-06 20:52:23 +0000

    Replaced excel demo with text version

--- examples/excel/excel_peg_spec.rb
+++ examples/excel/excel_peg_spec.rb
@@ -1,82 +1,87 @@
 $:.unshift File.join(File.dirname(__FILE__), *%w[.])
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. .. lib])
-require 'ruby_peg'
-require 'excel_peg'
+require 'text_peg2ruby_peg'
+text_peg = IO.readlines(File.join(File.dirname(__FILE__),'excel_peg.txt')).join
+ast = TextPeg.parse(text_peg)
+# puts ast.to_ast
+# exit
+builder = TextPeg2RubyPeg.new
+ruby = ast.build(builder)
+Kernel.eval(ruby)
 
-describe ExcelPeg do
+describe Formula do
   
   def check(text)
     puts
-    e = ExcelPeg.new
+    e = Formula.new
     e.parse(text)
     e.pretty_print_cache
     puts
   end
   
   it "returns formulas" do
-    ExcelPeg.parse('').to_ast.should == [:formula,""]
+    Formula.parse('1+1').to_ast.first.should == :formula
   end
   
   it "returns cells" do
-    ExcelPeg.parse('$A$1').to_ast.should == [:formula,[:cell,'$A$1']]
-    ExcelPeg.parse('A1').to_ast.should == [:formula,[:cell,'A1']]    
-    ExcelPeg.parse('$A1').to_ast.should == [:formula,[:cell,'$A1']]    
-    ExcelPeg.parse('A$1').to_ast.should == [:formula,[:cell,'A$1']]    
-    ExcelPeg.parse('AAA1123').to_ast.should == [:formula,[:cell,'AAA1123']]
+    Formula.parse('$A$1').to_ast.should == [:formula,[:cell,'$A$1']]
+    Formula.parse('A1').to_ast.should == [:formula,[:cell,'A1']]    
+    Formula.parse('$A1').to_ast.should == [:formula,[:cell,'$A1']]    
+    Formula.parse('A$1').to_ast.should == [:formula,[:cell,'A$1']]    
+    Formula.parse('AAA1123').to_ast.should == [:formula,[:cell,'AAA1123']]
   end
   
   it "returns areas" do
-    ExcelPeg.parse('$A$1:$Z$1').to_ast.should == [:formula,[:area,'$A$1','$Z$1']]
-    ExcelPeg.parse('A1:$Z$1').to_ast.should == [:formula,[:area,'A1','$Z$1']]    
+    Formula.parse('$A$1:$Z$1').to_ast.should == [:formula,[:area,'$A$1','$Z$1']]
+    Formula.parse('A1:$Z$1').to_ast.should == [:formula,[:area,'A1','$Z$1']]    
   end
   
   it "returns row ranges" do
-    ExcelPeg.parse('$1:$1000').to_ast.should == [:formula,[:row_range,'$1','$1000']]
-    ExcelPeg.parse('1000:1').to_ast.should == [:formula,[:row_range,'1000','1']]    
+    Formula.parse('$1:$1000').to_ast.should == [:formula,[:row_range,'$1','$1000']]
+    Formula.parse('1000:1').to_ast.should == [:formula,[:row_range,'1000','1']]    
   end
   
   it "returns column ranges" do
-    ExcelPeg.parse('$C:$AZ').to_ast.should == [:formula,[:column_range,'$C','$AZ']]
-    ExcelPeg.parse('C:AZ').to_ast.should == [:formula,[:column_range,'C','AZ']]    
+    Formula.parse('$C:$AZ').to_ast.should == [:formula,[:column_range,'$C','$AZ']]
+    Formula.parse('C:AZ').to_ast.should == [:formula,[:column_range,'C','AZ']]    
   end
   
   it "returns references to other sheets" do
-    ExcelPeg.parse('sheet1!$A$1').to_ast.should == [:formula,[:simple_sheet,'sheet1',[:cell,'$A$1']]]    
-    ExcelPeg.parse('sheet1!$A$1:$Z$1').to_ast.should == [:formula,[:simple_sheet,'sheet1',[:area,'$A$1','$Z$1']]]
-    ExcelPeg.parse('sheet1!$1:$1000').to_ast.should == [:formula,[:simple_sheet,'sheet1',[:row_range,'$1','$1000']]]
-    ExcelPeg.parse('sheet1!$C:$AZ').to_ast.should == [:formula,[:simple_sheet,'sheet1',[:column_range,'$C','$AZ']]]
+    Formula.parse('sheet1!$A$1').to_ast.should == [:formula,[:sheet_reference,'sheet1',[:cell,'$A$1']]]    
+    Formula.parse('sheet1!$A$1:$Z$1').to_ast.should == [:formula,[:sheet_reference,'sheet1',[:area,'$A$1','$Z$1']]]
+    Formula.parse('sheet1!$1:$1000').to_ast.should == [:formula,[:sheet_reference,'sheet1',[:row_range,'$1','$1000']]]
+    Formula.parse('sheet1!$C:$AZ').to_ast.should == [:formula,[:sheet_reference,'sheet1',[:column_range,'$C','$AZ']]]
   end
   
   it "returns references to other sheets with extended names" do
-    ExcelPeg.parse("'sheet 1'!$A$1").to_ast.should == [:formula,[:quoted_sheet,'sheet 1',[:cell,'$A$1']]]    
-    ExcelPeg.parse("'sheet 1'!$A$1:$Z$1").to_ast.should == [:formula,[:quoted_sheet,'sheet 1',[:area,'$A$1','$Z$1']]]
-    ExcelPeg.parse("'sheet 1'!$1:$1000").to_ast.should == [:formula,[:quoted_sheet,'sheet 1',[:row_range,'$1','$1000']]]
-    ExcelPeg.parse("'sheet 1'!$C:$AZ").to_ast.should == [:formula,[:quoted_sheet,'sheet 1',[:column_range,'$C','$AZ']]]
+    Formula.parse("'sheet 1'!$A$1").to_ast.should == [:formula,[:quoted_sheet_reference,'sheet 1',[:cell,'$A$1']]]    
+    Formula.parse("'sheet 1'!$A$1:$Z$1").to_ast.should == [:formula,[:quoted_sheet_reference,'sheet 1',[:area,'$A$1','$Z$1']]]
+    Formula.parse("'sheet 1'!$1:$1000").to_ast.should == [:formula,[:quoted_sheet_reference,'sheet 1',[:row_range,'$1','$1000']]]
+    Formula.parse("'sheet 1'!$C:$AZ").to_ast.should == [:formula,[:quoted_sheet_reference,'sheet 1',[:column_range,'$C','$AZ']]]
   end
   
   it "returns numbers" do
-    ExcelPeg.parse("1").to_ast.should == [:formula,[:number,'1']]
-    ExcelPeg.parse("103.287").to_ast.should == [:formula,[:number,'103.287']]
-    ExcelPeg.parse("-1.0E-27").to_ast.should == [:formula,[:number,'-1.0E-27']]
+    Formula.parse("1").to_ast.should == [:formula,[:number,'1']]
+    Formula.parse("103.287").to_ast.should == [:formula,[:number,'103.287']]
+    Formula.parse("-1.0E-27").to_ast.should == [:formula,[:number,'-1.0E-27']]
   end
 
   it "returns percentages" do
-    ExcelPeg.parse("1%").to_ast.should == [:formula,[:percentage,'1%']]
-    ExcelPeg.parse("103.287%").to_ast.should == [:formula,[:percentage,'103.287%']]
-    ExcelPeg.parse("-1.0%").to_ast.should == [:formula,[:percentage,'-1.0%']]
+    Formula.parse("1%").to_ast.should == [:formula,[:percentage,'1%']]
+    Formula.parse("103.287%").to_ast.should == [:formula,[:percentage,'103.287%']]
+    Formula.parse("-1.0%").to_ast.should == [:formula,[:percentage,'-1.0%']]
   end
   
   it "returns strings" do
-    ExcelPeg.parse('"A handy string"').to_ast.should == [:formula,[:string,"A handy string"]]
-    ExcelPeg.parse('"$A$1"').to_ast.should == [:formula,[:string,"$A$1"]]  
+    Formula.parse('"A handy string"').to_ast.should == [:formula,[:string,"A handy string"]]
+    Formula.parse('"$A$1"').to_ast.should == [:formula,[:string,"$A$1"]]  
   end
   
   it "returns string joins" do
-    check '"A handy string"&$A$1'
-    ExcelPeg.parse('"A handy string"&$A$1').to_ast.should == [:formula,[:string_join,[:string,"A handy string"],[:cell,'$A$1']]]
-    ExcelPeg.parse('$A$1&"A handy string"').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:string,"A handy string"]]]
-    ExcelPeg.parse('$A$1&"A handy string"&$A$1').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:string,"A handy string"],[:cell,'$A$1'],]]
-    ExcelPeg.parse('$A$1&$A$1&$A$1').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:cell,'$A$1'],[:cell,'$A$1'],]]
+    Formula.parse('"A handy string"&$A$1').to_ast.should == [:formula,[:string_join,[:string,"A handy string"],[:cell,'$A$1']]]
+    Formula.parse('$A$1&"A handy string"').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:string,"A handy string"]]]
+    Formula.parse('$A$1&"A handy string"&$A$1').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:string,"A handy string"],[:cell,'$A$1'],]]
+    Formula.parse('$A$1&$A$1&$A$1').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:cell,'$A$1'],[:cell,'$A$1'],]]
     
   end
   
