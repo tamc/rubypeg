09e399824f3fd8c124bb499b6bc97c973336e635
tamc <tamc@greenonblack.com>
2010-02-08 19:37:43 +0000

    Updated excel example to include named references and the like

--- examples/excel/excel_peg_spec.rb
+++ examples/excel/excel_peg_spec.rb
@@ -67,9 +67,9 @@ describe Formula do
   end
 
   it "returns percentages" do
-    Formula.parse("1%").to_ast.should == [:formula,[:percentage,'1%']]
-    Formula.parse("103.287%").to_ast.should == [:formula,[:percentage,'103.287%']]
-    Formula.parse("-1.0%").to_ast.should == [:formula,[:percentage,'-1.0%']]
+    Formula.parse("1%").to_ast.should == [:formula,[:percentage,'1']]
+    Formula.parse("103.287%").to_ast.should == [:formula,[:percentage,'103.287']]
+    Formula.parse("-1.0%").to_ast.should == [:formula,[:percentage,'-1.0']]
   end
   
   it "returns strings" do
@@ -82,7 +82,58 @@ describe Formula do
     Formula.parse('$A$1&"A handy string"').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:string,"A handy string"]]]
     Formula.parse('$A$1&"A handy string"&$A$1').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:string,"A handy string"],[:cell,'$A$1'],]]
     Formula.parse('$A$1&$A$1&$A$1').to_ast.should == [:formula,[:string_join,[:cell,'$A$1'],[:cell,'$A$1'],[:cell,'$A$1'],]]
-    
+    Formula.parse('"GW"&ISERR($AA$1)').to_ast.should == [:formula,[:string_join,[:string,'GW'],[:function,'ISERR',[:cell,'$AA$1']]]]
+  end
+  
+  it "returns numeric operations" do
+    Formula.parse('$A$1+$A$2+1').to_ast.should == [:formula,[:arithmetic,[:cell,'$A$1'],[:operator,"+"],[:cell,'$A$2'],[:operator,"+"],[:number,'1']]]
+    Formula.parse('$A$1-$A$2-1').to_ast.should == [:formula,[:arithmetic,[:cell,'$A$1'],[:operator,"-"],[:cell,'$A$2'],[:operator,"-"],[:number,'1']]]
+    Formula.parse('$A$1*$A$2*1').to_ast.should == [:formula,[:arithmetic,[:cell,'$A$1'],[:operator,"*"],[:cell,'$A$2'],[:operator,"*"],[:number,'1']]]
+    Formula.parse('$A$1/$A$2/1').to_ast.should == [:formula,[:arithmetic,[:cell,'$A$1'],[:operator,"/"],[:cell,'$A$2'],[:operator,"/"],[:number,'1']]            ]
+    Formula.parse('$A$1^$A$2^1').to_ast.should == [:formula,[:arithmetic,[:cell,'$A$1'],[:operator,"^"],[:cell,'$A$2'],[:operator,"^"],[:number,'1']]]
+  end
+  
+  it "returns expressions in brackets" do
+    Formula.parse('($A$1+$A$2)').to_ast.should == [:formula,[:brackets,[:arithmetic,[:cell,'$A$1'],[:operator,"+"],[:cell,'$A$2']]]]
+    Formula.parse('($A$1+$A$2)+2').to_ast.should == [:formula, [:arithmetic, [:brackets, [:arithmetic, [:cell,'$A$1'], [:operator,"+"], [:cell,'$A$2']]], [:operator,"+"], [:number,'2']]]
+    Formula.parse('($A$1+$A$2)+(2+(1*2))').to_ast.should == [:formula, [:arithmetic, [:brackets, [:arithmetic, [:cell,'$A$1'], [:operator,"+"], [:cell,'$A$2']]], [:operator,"+"], [:brackets, [:arithmetic, [:number,'2'], [:operator,'+'] ,[:brackets, [:arithmetic, [:number,'1'], [:operator,'*'], [:number,'2']]]]]]]  
+  end
+  
+  it "returns comparisons" do
+    Formula.parse('$A$1>$A$2').to_ast.should  == [:formula,[:comparison,[:cell,'$A$1'],[:comparator,">"],[:cell,'$A$2']]]
+    Formula.parse('$A$1<$A$2').to_ast.should  == [:formula,[:comparison,[:cell,'$A$1'],[:comparator,"<"],[:cell,'$A$2']]]
+    Formula.parse('$A$1=$A$2').to_ast.should  == [:formula,[:comparison,[:cell,'$A$1'],[:comparator,"="],[:cell,'$A$2']]]
+    Formula.parse('$A$1>=$A$2').to_ast.should == [:formula,[:comparison,[:cell,'$A$1'],[:comparator,">="],[:cell,'$A$2']]]
+    Formula.parse('$A$1<=$A$2').to_ast.should == [:formula,[:comparison,[:cell,'$A$1'],[:comparator,"<="],[:cell,'$A$2']]]
+    Formula.parse('$A$1<>$A$2').to_ast.should == [:formula,[:comparison,[:cell,'$A$1'],[:comparator,"<>"],[:cell,'$A$2']]]
+  end
+  
+  it "returns functions" do
+    Formula.parse('PI()').to_ast.should  == [:formula,[:function,'PI']]
+    Formula.parse('ERR($A$1)').to_ast.should  == [:formula,[:function,'ERR',[:cell,'$A$1']]]
+    Formula.parse('SUM($A$1,sheet1!$1:$1000,1)').to_ast.should  == [:formula,[:function,'SUM',[:cell,'$A$1'],[:sheet_reference,'sheet1',[:row_range,'$1','$1000']],[:number,'1']]]
+  end
+  
+  it "returns fully qualified structured references (i.e., Table[column])" do
+    Formula.parse('DeptSales[Sale Amount]').to_ast.should  == [:formula,[:qualified_table_reference,'DeptSales','Sale Amount']]
+    #Formula.parse("DeptSales[Sale'] Amount]").to_ast.should  == [:formula,[:qualified_table_reference,'DeptSales','Sale Amount']]
+  end
+  
+  it "returns booleans" do
+    Formula.parse("TRUE*FALSE").to_ast.should == [:formula,[:arithmetic,[:boolean_true],[:operator,'*'],[:boolean_false]]]
+  end
+  
+  it "returns prefixes (+/-)" do
+    Formula.parse("-(3-1)").to_ast.should == [:formula,[:prefix,'-'],[:brackets,[:arithmetic,[:number,'3'],[:operator,'-'],[:number,'1']]]]
+  end
+  
+  it "returns local structured references (i.e., [column])" do
+    Formula.parse('[Sale Amount]').to_ast.should  == [:formula,[:local_table_reference,'Sale Amount']]
+    #Formula.parse("DeptSales[Sale'] Amount]").to_ast.should  == [:formula,[:qualified_table_reference,'DeptSales','Sale Amount']]
+  end
+  
+  it "returns named references" do
+    Formula.parse('EF.NaturalGas.N2O').to_ast.should == [:formula,[:named_reference,'EF.NaturalGas.N2O']]
   end
   
 end
